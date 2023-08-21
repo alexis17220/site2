e02bb76cbd4a43f530a80fb4448dd55e9b55714c
Meysson17Alexis <117274846+Meysson17Alexis@users.noreply.github.com>
2023-08-21 10:50:00 +0200

    Initial commit

---
+++ src/components/Home/Home2.js
@@ -0,0 +1,94 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/avatar.svg";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Home2() {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              Laisse moi <span className="purple"> me présenter</span>
+            </h1>
+            <p className="home-about-body">
+            Je suis capable de m'exprimer aisément en utilisant des technologies classiques telles que
+              <i>
+                <b className="purple"> ReactJS et Python. </b>
+              </i>
+              <br />
+              <br />
+              Mon activité principale consiste davantage à concevoir, déboguer et résoudre des problèmes qu'à simplement coder.
+              <br />
+              <br />
+              
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={myImg} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>Vous pouvez me retrouver en consultant les informations suivantes.</h1>
+            <p>
+            N'hésitez aucunement à entrer en <span className="purple">communication </span> avec moi.
+            </p>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/sukrutrahane"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://twitter.com/sukrutrahane"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiOutlineTwitter />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/sukrutrahane/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.instagram.com/sukkkrut"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <AiFillInstagram />
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+export default Home2;
